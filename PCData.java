a6942a4ebefe6bcb5a02725cd9c18fec384b0076
Kuo Li <constlk@163.com>
2018-01-12 18:11:53 +0800

    缓冲区数据类型

---
+++ PCData.java
@@ -0,0 +1,16 @@
+package pc;
+/**
+ * 缓冲区类型
+ * @author Kuo
+ *
+ */
+public class PCData {
+private int data;
+
+public int getData() {
+	return data;
+}
+public void setData(int data){
+	this.data=data;
+}
+}
